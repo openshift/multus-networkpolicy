k8snetworkplumbingwg/multi-networkpolicy-nftables

logging: Print info at startup
Dump operating info like the hostname and the common
rules when starting, as they could be useful for debugging purpose.

Signed-off-by: Andrea Panattoni <apanatto@redhat.com>

cmd/main.go
@@ -77,6 +77,7 @@ func run() error {
 	if err != nil {
 		return fmt.Errorf("unable to get hostname: %w", err)
 	}
+	setupLog.Info("Handling pods for", "node", hostname)
 
 	if criEndpoint == "" {
 		return fmt.Errorf("container-runtime-endpoint must be set")
@@ -103,6 +104,8 @@ func run() error {
 	// Set ICMP acceptance rules
 	commonRules.AcceptICMP = acceptICMP
 	commonRules.AcceptICMPv6 = acceptICMPv6
+
+	setupLog.Info("Common rules applied to all pods affected ny MultinetPolicies", "rules", commonRules)
 
 	ctx := ctrl.SetupSignalHandler()
 

openshift/multus-networkpolicy

cmd/main.go
@@ -77,6 +77,7 @@ func run() error {
 	if err != nil {
 		return fmt.Errorf("unable to get hostname: %w", err)
 	}
+	setupLog.Info("Handling pods for", "node", hostname)
 
 	if criEndpoint == "" {
 		return fmt.Errorf("container-runtime-endpoint must be set")
@@ -103,6 +104,8 @@ func run() error {
 	// Set ICMP acceptance rules
 	commonRules.AcceptICMP = acceptICMP
 	commonRules.AcceptICMPv6 = acceptICMPv6
+
+	setupLog.Info("Common rules applied to all pods affected by MultiNetworkPolicies", "rules", commonRules)
 
 	ctx := ctrl.SetupSignalHandler()
 